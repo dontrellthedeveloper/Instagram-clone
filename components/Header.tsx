27d9e16bfe66ae3a906e1236fc287019b6f69fb6
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-07-07 14:36:02 -0400

    started to build middle part of header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Image from "next/image";
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon
+} from "@heroicons/react/solid";
+
 
 function Header() {
     return (
         <div>
             <div className='flex justify-between bg-white max-w-6xl'>
         {/*  Left  */}
-            <div className='relative hidden lg:inline-grid h-24 w-24 cursor-pointer'>
+            <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
                 <Image
                     src="https://links.papareact.com/ocw"
                     layout='fill'
@@ -14,7 +23,7 @@ function Header() {
                 />
             </div>
 
-            <div className='relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer'>
+            <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
                 <Image
                     src="https://links.papareact.com/jjm"
                     layout='fill'
@@ -22,9 +31,13 @@ function Header() {
                 />
             </div>
 
-
-
         {/*  Middle  */}
+            <div className='relative mt-1 p-3 rounded-md'>
+                <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+                    <SearchIcon className='h-5 w-5 text-gray-500'/>
+                </div>
+                <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' type="text" placeholder="Search" />
+            </div>
 
         {/*  Right  */}
 
