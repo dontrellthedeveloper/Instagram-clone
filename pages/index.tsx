8b06253a4ba9f68bfe8e52270fa3be9ac482c3bb
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-07-07 21:55:02 -0400

    started to create the basic structure of the feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import Feed from "../components/Feed";
 
 const Home: NextPage = () => {
   return (
@@ -15,7 +16,7 @@ const Home: NextPage = () => {
         <Header />
 
       {/* Feed */}
-
+        <Feed/>
       {/* Modal */}
     </div>
   )
