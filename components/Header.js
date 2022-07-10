a9719ae1008e88c9a9681d3cd3688d33519d149b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-07-09 22:00:23 -0400

    added the modal component

--- components/Header.js
+++ components/Header.js
@@ -12,10 +12,13 @@ import {
 import {HomeIcon} from "@heroicons/react/solid"
 import {useSession, signIn, signOut} from "next-auth/react";
 import {useRouter} from "next/router";
+import {useRecoilState} from "recoil";
+import {modalState} from "../atoms/modalAtom";
 
 
 function Header() {
     const {data: session} = useSession();
+    const [open, setOpen] = useRecoilState(modalState);
     const router = useRouter();
 
     console.log(session)
@@ -60,7 +63,7 @@ function Header() {
                                 <PaperAirplaneIcon className='navBtn rotate-45' />
                                 <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
                             </div>
-                            <PlusCircleIcon className='navBtn' />
+                            <PlusCircleIcon onClick={() => setOpen(true)} className='navBtn' />
                             <UserGroupIcon className='navBtn' />
                             <HeartIcon className='navBtn' />
 
