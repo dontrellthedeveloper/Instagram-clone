267bf1b4bd2d1cc1ca74231c8bb5b393036a2220
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-07-09 23:24:39 -0400

    added the post compnent and added the ability to see the post from firebase in the front end. Also added session state to post component.

--- components/Post.js
+++ components/Post.js
@@ -9,8 +9,10 @@ import {
 } from "@heroicons/react/outline";
 
 import {HeartIcon as HeartIconFilled} from '@heroicons/react/solid'
+import {useSession} from "next-auth/react";
 
 function Post({id, username, userImg, caption, img}) {
+    const {data: session} = useSession();
     return (
         <div className='bg-white my-7 border rounded-sm'>
             {/* Header */}
@@ -24,15 +26,18 @@ function Post({id, username, userImg, caption, img}) {
             <img src={img} className='object-cover w-full' alt=''/>
 
             {/*  Button  */}
-            <div className='flex justify-between px-4 pt-4'>
-                <div className='flex space-x-4'>
-                    <HeartIcon className='btn' />
-                    <ChatIcon className='btn' />
-                    <PaperAirplaneIcon className='btn' />
-                </div>
+            {session && (
+                <div className='flex justify-between px-4 pt-4'>
+                    <div className='flex space-x-4'>
+                        <HeartIcon className='btn' />
+                        <ChatIcon className='btn' />
+                        <PaperAirplaneIcon className='btn' />
+                    </div>
 
-                <BookmarkIcon className='btn' />
-            </div>
+                    <BookmarkIcon className='btn' />
+                </div>
+            )}
+
 
 
             {/*  caption  */}
@@ -46,12 +51,14 @@ function Post({id, username, userImg, caption, img}) {
 
 
             {/*  input box  */}
+            {session && (
             <form className='flex items-center p-4'>
                 <EmojiHappyIcon className='h-7' />
 
                 <input type="text" placeholder='add a comment...' className='border-none flex-1 focus:ring-0 outline-none'/>
                 <button className='font-semibold text-blue-400'>Post</button>
             </form>
+            )}
 
         </div>
 
--- components/Posts.js
+++ components/Posts.js
@@ -1,56 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {collection, onSnapshot, orderBy, query} from "@firebase/firestore";
+import {db, storage} from "../firebase";
 import Post from "./Post";
 
-const posts = [
-    {
-        id: '123',
-        username: 'dontrellthedev',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'SUBSCRIBE AND DESTROY THE LIKE BUTTON for the YT algorithm'
-    },
-    {
-        id: '123',
-        username: 'dontrellthedev',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'SUBSCRIBE AND DESTROY THE LIKE BUTTON for the YT algorithm'
-    },
-    {
-        id: '123',
-        username: 'dontrellthedev',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'SUBSCRIBE AND DESTROY THE LIKE BUTTON for the YT algorithm'
-    },
-    {
-        id: '123',
-        username: 'dontrellthedev',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'SUBSCRIBE AND DESTROY THE LIKE BUTTON for the YT algorithm'
-    },
-    {
-        id: '123',
-        username: 'dontrellthedev',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'SUBSCRIBE AND DESTROY THE LIKE BUTTON for the YT algorithm'
-    },
-]
+
 
 
 function Posts(props) {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() =>
+         onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+            setPosts(snapshot.docs);
+            }
+        )
+    , [db]);
+
+    console.log(posts);
+
     return (
         <div>
             {posts.map((post) => (
                 <Post
                     key={post.id}
                     id={post.id}
-                    username={post.username}
-                    userImg={post.userImg}
-                    img={post.img}
-                    caption={post.caption}
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
                 />
             ))}
             <Post/>
